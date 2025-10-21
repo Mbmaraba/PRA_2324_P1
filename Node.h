4ad3fe7d369014428fefa2c7f572ec01feaf2325
MBMARABA <mbmaraba@alumno.upv.es@ldsic-vdi07.upvnet.upv.es>
2025-10-21 21:24:56 +0200

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,35 @@
+#ifndef NODE_H
+#define NODE_H
+#include <ostream>
+
+template <typename T>
+class Node {
+    public:
+        T data;//elemento amacenado
+        Node<T> * next; //puntero al siguiente nodo
+
+        //constructor
+        Node(T data, Node<T>*next=nullptr){
+            this->data = data;
+            this-> next = next;
+        }
+
+        Node(T data){
+            this->data = data;
+            this->next = nullptr;
+
+        }
+      // Sobrecarga global del operador << (plantilla amiga)
+    template <typename U>
+    friend std::ostream& operator<<(std::ostream& out, const Node<U>& node);
+};
+
+// Implementación del operador <<
+// Imprime solo el atributo 'data'
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+    out << node.data;
+    return out;
+}
+
+#endif // NODE_H
